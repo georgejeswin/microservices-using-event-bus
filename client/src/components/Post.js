4305e9386d68afaa035e95e47c91094a7856ffd5
georgejeswin <jeswinmyladoor@gmail.com>
2023-03-16 23:59:12 +0530

    Event bus express app created

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -13,6 +13,7 @@ const Post = ({ post }) => {
 
   useEffect(() => {
     getComments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
---
+++ event-bus/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axiox";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = 4005;
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/api/events", event);
+  axios.post("http://localhost:4001/api/events", event);
+  axios.post("http://localhost:4002/api/events", event);
+
+  res.send({ status: "OK" });
+});
+
+app.listen(PORT, () => console.log("App listening on PORT", PORT));
